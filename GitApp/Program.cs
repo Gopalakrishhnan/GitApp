c3588abc662195e00e628ff72ad9a322fb44afdc
Gopalakrishhnan <96166552+Gopalakrishhnan@users.noreply.github.com>
2022-07-23 20:20:58 +0530

    change a=20

--- GitApp/Program.cs
+++ GitApp/Program.cs
@@ -6,7 +6,7 @@ namespace GitApp
     {
         static void Main(string[] args)
         {
-            int a = 1;
+            int a = 20;
             int e = 22;
             int c = a + e;
             Console.WriteLine(c);
