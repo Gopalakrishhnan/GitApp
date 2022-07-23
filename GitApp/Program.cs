b371869e22b347543c26e36ec0e37034ed8d01de
Gopalakrishhnan <96166552+Gopalakrishhnan@users.noreply.github.com>
2022-07-23 20:37:09 +0530

    a=104

--- GitApp/Program.cs
+++ GitApp/Program.cs
@@ -6,7 +6,7 @@ namespace GitApp
     {
         static void Main(string[] args)
         {
-            int a = 103;
+            int a = 104;
             int e = 22;
             int c = a + e;
             Console.WriteLine(c);
