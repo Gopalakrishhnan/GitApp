ee6cc8a0128c181726a62d420bfe7a20eb60c741
GOPALAKRISHNAN <gopalakrishnan161ca117@gmail.com>
2022-07-23 20:32:05 +0530

    a=102

--- GitApp/Program.cs
+++ GitApp/Program.cs
@@ -6,7 +6,7 @@ namespace GitApp
     {
         static void Main(string[] args)
         {
-            int a = 101;
+            int a = 102;
             int e = 22;
             int c = a + e;
             Console.WriteLine(c);
